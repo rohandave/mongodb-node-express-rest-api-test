3996854d6eaaed9d2bbb88c99a64b6bd3645a57b
rohandave <rohan.dave778@gmail.com>
2023-11-02 20:30:25 +0530

    update

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const app = require('./src')
-const port = '8001'; //try to pick from config file
+const config = require('./src/config');
+const port = config.port;
 
 app.listen(port, () => {
     console.log(`Backend REST API running on Port - ${port}`);
--- src/config/index.js
+++ src/config/index.js
@@ -4,4 +4,5 @@ module.exports = {
         password: '<dbPassword>',
         database: 'dbBookManagement',
     },
+    port: '8001',
 }
